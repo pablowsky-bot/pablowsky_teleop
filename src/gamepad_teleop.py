a6a7654d6b53b4e10b0def2510b0f0cbd7c79cb5
Oscar Lima <oscar.lima@dfki.de>
2022-03-13 19:01:00 +0100

    gamepad teleop node initial code upload

---
+++ src/gamepad_teleop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+cmd_vel_msg = Twist()
+
+cmd_vel_msg.linear.x = 0.0
+cmd_vel_msg.linear.y = 0.0
+cmd_vel_msg.linear.z = 0.0
+
+cmd_vel_msg.angular.x = 0.0
+cmd_vel_msg.angular.y = 0.0
+cmd_vel_msg.angular.z = 0.0
+
+def callback(msg):
+    cmd_vel_msg.linear.x = msg.axes[1]
+    cmd_vel_msg.angular.z = msg.axes[2]
+
+def gamepad_teleop():
+    rospy.init_node('gamepad_teleop', anonymous=True)
+    rospy.loginfo('pablowsky gamepad teleop node started')
+    rospy.Subscriber("joy", Joy, callback)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        pub.publish(cmd_vel_msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        gamepad_teleop()
+    except rospy.ROSInterruptException:
+        pass
